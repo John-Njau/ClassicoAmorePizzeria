429bcbb62997c5406ead195986b0b9fbe8d230c4
John <john.njoroge@student.moringaschool.com>
2022-03-19 20:32:34 +0300

    ft:Button functionality

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -13,16 +13,37 @@
 // }
 
 $().ready(function () {
+    $("#delivery").click(function () {
+        $("#delivery-location").show();
+        $("#totalPayment").show();
+        $("#pick-up").hide();
+    });
     $("#totalPayment").click(function (event) {
         event.preventDefault();
 
         var location = $('#location').val();
+        var phoneNumber = $('#phone-no').val();
 
-        if (location != "") {
-            alert("Your Delivery will be Dispatched to " + location)
-        }
-        else {
-            alert("Please Enter your Location")
+        if (location != "" && phoneNumber != "") {
+            alert("Your Payment of *** has been received.Your Delivery will be Dispatched to " + location + ". Enjoy!")
+        } else {
+            alert("Please Enter Your Delivery Details")
         }
     });
+    
+});
+
+$().ready(function () {
+    $("#pick-up").click(function () {
+        $("#delivery").hide();
+        $("#totalPayment").hide();
+        $("#totalAmount").show();
+
+    });
+        $("#totalAmount").click(function () {
+            // var amount = $('#location').val();
+
+            alert("Your Payment of *** has been received. Your order will be available for pick up in 20mins")
+
+        });
 });
